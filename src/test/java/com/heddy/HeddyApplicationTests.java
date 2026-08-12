3f23b78bc230230df74a0d7367581697244ace43
hoshino-love0102 <hoshino.whale0102@gmail.com>
2026-08-12 21:00:40 +0900

    test: 애플리케이션 기본 테스트 추가

---
+++ src/test/java/com/heddy/HeddyApplicationTests.java
@@ -0,0 +1,14 @@
+package com.heddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class HeddyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}

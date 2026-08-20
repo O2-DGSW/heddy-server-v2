4d75ed8cd1194f2895264ed877bc3f2522d6bf5a
hoshino-love0102 <hoshino.whale0102@gmail.com>
2026-08-20 21:18:23 +0900

    feat: 모발 프로필 영속성 기반 추가

---
+++ src/main/java/com/heddy/domain/account/port/out/HairProfileRepositoryPort.java
@@ -0,0 +1,11 @@
+package com.heddy.domain.account.port.out;
+
+import com.heddy.domain.account.model.HairProfile;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface HairProfileRepositoryPort {
+    Optional<HairProfile> findByUserId(UUID userId);
+    HairProfile save(HairProfile hairProfile);
+}

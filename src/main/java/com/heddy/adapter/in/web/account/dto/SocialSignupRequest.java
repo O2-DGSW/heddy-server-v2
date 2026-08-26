9fdc74f87f2c59a7ce87a10ff3d3dae2671ad113
hoshino-love0102 <hoshino.whale0102@gmail.com>
2026-08-26 10:39:27 +0900

    docs: 인증 요청 스키마 설명 보완 (#87)

--- src/main/java/com/heddy/adapter/in/web/account/dto/SocialSignupRequest.java
+++ src/main/java/com/heddy/adapter/in/web/account/dto/SocialSignupRequest.java
@@ -3,14 +3,19 @@ package com.heddy.adapter.in.web.account.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.heddy.domain.account.model.AuthProvider;
 import com.heddy.domain.account.port.in.SocialSignupCommand;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import jakarta.validation.constraints.Pattern;
 
+@Schema(description = "소셜 회원가입 요청")
 public record SocialSignupRequest(
-        @NotNull AuthProvider provider,
+        @NotNull
+        @Schema(description = "소셜 인증 제공자",
+                allowableValues = {"GOOGLE", "KAKAO", "APPLE"})
+        AuthProvider provider,
         @NotBlank @Size(max = 4096) @JsonProperty("provider_token") String providerToken,
         @NotBlank @Size(max = 30) String nickname,
         @Pattern(regexp = "^01[016789]\\d{7,8}$") @JsonProperty("phone_number") String phoneNumber,
